d1b200002009409d3038d5411898b7ccb1e22bf4
seongjju <jeongsj1226@naver.com>
2024-05-07 13:11:24 +0900

    mod:test_last

--- src/test/java/web_server/web/socket/WebSocketApplicationTests.java
+++ src/test/java/web_server/web/socket/WebSocketApplicationTests.java
@@ -1,13 +1,12 @@
 package web_server.web.socket;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class WebSocketApplicationTests {
 
 	@Test
 	void contextLoads() {
+		// 특별한 설정이나 동작 없이 Spring 애플리케이션 컨텍스트가 로드되는지만 확인합니다.
 	}
-
 }
